efd7b3f15b1d0336f5fc15d592e34c0bf612a22e
Alexcatprg <85404592+Alexcatprg@users.noreply.github.com>
2022-05-23 21:30:51 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+es = [5, 2, 3, 1, 4]
+print(es)
+i = int(input('Выберите первое число от 1 до 5: '))
+m = [3, 1, 7, 8, 6]
+print(m)
+u = int(input('Введите второе число:'))
+test = i + u
+e =[4, 3, 5, 2]
+print(e)
+w = int(input('Введите третье число:'))
+res = test * w
+print(res)
